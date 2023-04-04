6f90f6d30a67c32070fa340a20d8e4e71853ec85
prabh <prabhjeetsokhey@gmail.com>
2023-04-03 20:13:08 -0700

    cleaned fronted to look more professional.

--- web/script.js
+++ web/script.js
@@ -1,5 +1,6 @@
 const postButton = document.getElementById('post-button');
 const getButton = document.getElementById('get-button');
+const deleteButton = document.getElementById('delete-button');
 const outputDiv = document.getElementById('output');
 
 // Send a POST request to the server
@@ -14,27 +15,40 @@ function postData() {
     },
     body: JSON.stringify(data)
   })
-  .then(response => response.json())
-  .then(response => {
-    console.log(response);
-    outputDiv.innerHTML = 'Data posted successfully!';
-  })
-  .catch(error => console.error(error));
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        outputDiv.innerHTML = 'Data posted successfully!';
+      })
+      .catch(error => console.error(error));
 }
 
 // Send a GET request to the server
 function getData() {
   const baseUrl = window.location.href.split('/')[0]; // get the base URL
   fetch(baseUrl + 'getDatabase') // use the base URL for the request
-  .then(response => response.text())
-  .then(data => {
-    console.log(data);
-    outputDiv.innerHTML = data;
-  })
-  .catch(error => console.error(error));
+      .then(response => response.text())
+      .then(data => {
+        console.log(data);
+        outputDiv.innerHTML = data;
+      })
+      .catch(error => console.error(error));
 }
 
+// Send a DELETE request to the server
+function deleteData() {
+  fetch(window.location.href + 'DELETE', {
+    method: 'DELETE'
+  })
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        outputDiv.innerHTML = 'Database deleted successfully!';
+      })
+      .catch(error => console.error(error));
+}
 
 postButton.addEventListener('click', postData);
 getButton.addEventListener('click', getData);
+deleteButton.addEventListener('click', deleteData);
 
